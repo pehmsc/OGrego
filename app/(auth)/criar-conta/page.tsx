b4682219b2c2edce02ce765dfd1a3a98c9582a8a
pehmsc <86537241+pehmsc@users.noreply.github.com>
2026-02-02 22:46:32 +0000

    fix button layout and links

--- app/(auth)/criar-conta/page.tsx
+++ app/(auth)/criar-conta/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { SignupForm } from "@/components/signup-form";
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-[var(--background)] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="self-center">
           <div className="relative h-12 w-40">
--- app/(auth)/entrar/page.tsx
+++ app/(auth)/entrar/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { LoginForm } from "@/components/login-form";
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-[var(--background)] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="self-center">
           <div className="relative h-12 w-40">
