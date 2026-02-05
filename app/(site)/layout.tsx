4d953c838b2a30ac794289aa55c19da1288aaa0a
pehmsc <86537241+pehmsc@users.noreply.github.com>
2026-02-05 14:50:50 +0000

    HeaderUser

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,14 +1,44 @@
 import type { ReactNode } from "react";
-import Header from "../ui/components/Header";
-import Footer from "../ui/components/Footer";
+import { auth } from "@clerk/nextjs/server";
 
-export default function SiteLayout({ children }: { children: ReactNode }) {
+import Header from "../ui/components/Header";
+import HeaderUser from "@/app/ui/components/HeaderUser";
+import Footer from "../ui/components/Footer";
+import { getCurrentUserDb } from "@/app/lib/current-user";
+
+export default async function SiteLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { userId } = await auth();
+
+  // Não autenticado -> Header público
+  if (!userId) {
+    return (
+      <div className="flex min-h-screen flex-col bg-[var(--background)]">
+        <Header />
+        <main className="mx-auto max-w-7xl flex-1 px-6 pt-28 pb-10">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+
+  // Autenticado -> role vem da BD
+  const dbUser = await getCurrentUserDb();
+
+  const showUserHeader = dbUser.role === "user"; // admin -> não mostra
+
   return (
     <div className="flex min-h-screen flex-col bg-[var(--background)]">
-      <Header />
-      <main className="mx-auto max-w-7xl flex 1 px-6 pt-28 pb-10">
+      {showUserHeader ? <HeaderUser /> : null}
+
+      <main className="mx-auto max-w-7xl flex-1 px-6 pt-28 pb-10">
         {children}
       </main>
+
       <Footer />
     </div>
   );
