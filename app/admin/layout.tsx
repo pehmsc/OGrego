8945f8d23cc0abe50cdea1355fa4d867d46cf444
VidiPT89 <damartins89@gmail.com>
2026-02-10 15:18:02 +0000

    Dashboard duplicado resolvido pela chefe Joana

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,17 +2,17 @@ import AdminSideNav from "@/app/ui/components/AdminSideNav";
 import AdminHeader from "@/app/ui/components/AdminHeader";
 
 export default function AdminLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen bg-gray-50">
-      <AdminSideNav />
-      <div className="flex flex-1 flex-col">
-        <AdminHeader />
-        <main className="p-6">{children}</main>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen bg-gray-50">
+            <AdminSideNav />
+            <div className="flex flex-1 flex-col">
+                <AdminHeader />
+                <main className="p-6">{children}</main>
+            </div>
+        </div>
+    );
 }
